a5344720861b43a68bc4dd3871021cc070df1771
Rushi Chandalia <56991795+RushiChandalia@users.noreply.github.com>
2020-12-20 15:34:44 +0530

    Styling done
    Assignment Complete

---
+++ ASP.net Assignments/SourceControlFinalAssingment/Drop dbo.UserTable.sql
@@ -0,0 +1,13 @@
+﻿USE [master]
+GO
+
+/****** Object: Table [dbo].[UserTable] Script Date: 20-12-2020 15:17:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DELETE FROM [dbo].[UserTable];
+
+
