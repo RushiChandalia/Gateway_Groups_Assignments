25dda5aeb6fea8807b4a80e7c5563e33e07b23e6
Rushi Chandalia <56991795+RushiChandalia@users.noreply.github.com>
2021-01-01 11:16:46 +0530

    deleted the product models and views

--- MVC_Product_management_Project/MVC_Product_management_Project/Controllers/HomeController.cs
+++ MVC_Product_management_Project/MVC_Product_management_Project/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace MVC_Product_management_Project.Controllers
 {
     public class HomeController : Controller
     {
-        private ProductsDBContext db = new ProductsDBContext();
+      
         // GET: Home
         public ActionResult HomeIndex()
         {
@@ -20,61 +20,7 @@ namespace MVC_Product_management_Project.Controllers
             Session.Abandon();
             return RedirectToAction("Index", "Login");
         }
-        [HttpGet]
-        public ActionResult AddProduct()
-        {
-            return View();
-        }
-
-        public ActionResult productList()
-        {
-            var Products = from e in db.Products
-                           orderby e.ProductId
-                           select e;
-            return View(Products);
-        }
-        [HttpPost]
-        public ActionResult AddProduct(Products pro)
-        {
-            try
-            {
-                db.Products.Add(pro);
-                db.SaveChanges();
-                return RedirectToAction("productList");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            var employee = db.Products.Single(m => m.ProductId == id);
-            return View(employee);
-        }
-
-        // POST: Employee/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                var employee = db.Products.Single(m => m.ProductId == id);
-                if (TryUpdateModel(employee))
-                {
-                    //To Do:- database code
-                    db.SaveChanges();
-                    return RedirectToAction("productList");
-                }
-                return View(employee);
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+       
 
 
     }
