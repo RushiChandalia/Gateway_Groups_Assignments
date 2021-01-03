4d17e04b6c3b906bb310cdfc29d9ea6bb289ff67
Rushi Chandalia <56991795+RushiChandalia@users.noreply.github.com>
2021-01-03 18:26:59 +0530

    started web api

---
+++ MVC_Product_management_Project/MVC_Product_management_Project/Models/products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Product_management_Project.Models
+{
+    public class products
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public double? Price { get; set; }
+
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        public int? Quantity { get; set; }
+
+        [Column("Short Description")]
+        [StringLength(50)]
+        public string Short_Description { get; set; }
+
+        [Column("Long Description")]
+        [StringLength(100)]
+        public string Long_Description { get; set; }
+
+        [Column("Small Image")]
+        [StringLength(50)]
+        public string Small_Image { get; set; }
+
+        [Column("Large Image")]
+        [StringLength(50)]
+        public string Large_Image { get; set; }
+    }
+}
